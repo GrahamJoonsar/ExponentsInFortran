31e0d4c9f4badcf715e0c9cd2343dcfa91318e10
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-04-08 23:19:43 -0400

    Add files via upload

---
+++ main.f90
@@ -0,0 +1,16 @@
+
+program hello
+    implicit none
+
+    real :: num, pow
+
+    print *, "Number to be exponential: "
+    read *, num
+    print *, "The exponential: "
+    read *, pow
+    print *, num ** pow
+    print *, "Press enter to exit"
+    read *,
+
+end program
+
